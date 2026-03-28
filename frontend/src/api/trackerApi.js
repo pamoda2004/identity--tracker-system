a155eb7fd1b5da55f1717027215312d71d96104a
Pamoda Jayathilaka <jayathilakapamoda9@gmail.com>
2026-03-28 16:07:37 +0530

    Revert "Add dashboard into main"

--- frontend/src/api/trackerApi.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-import { API_BASE } from "../utils/constants";
-
-export const getFrame = () => axios.get(`${API_BASE}/tracker/frame`);
-export const getActiveIds = () => axios.get(`${API_BASE}/tracker/active-ids`);
